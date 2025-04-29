255d1cc7d55dbac35109f5f0267311ae1294e589
Emily Oliveira <ewileet@gmail.com>
2025-04-29 18:44:37 -0300

    Criação do Heading.module.css, explicação a respeito do escopo e uso de variáveis em React e adição das roots de cores para uso do projeto.

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,12 @@ import './styles/global.css'
 export function App() {
     return (
         <> { /* --> react fragment */ }
-            <Heading />
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum nobis qui, voluptates doloribus nam dolores dicta nisi laborum pariatur blanditiis doloremque fuga. Dolorum beatae, inventore cum tenetur quaerat cupiditate eligendi?</p>
+            <Heading attr2={123} attr='String'>
+                Olá, mundo!
+            </Heading>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum nobis qui, voluptates doloribus nam dolores dicta nisi laborum pariatur blanditiis doloremque fuga. Dolorum beatae, inventore cum tenetur quaerat cupiditate eligendi?
+            </p>
         </>
     )
 }
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,3 +1,5 @@
-export function Heading() {
-    return <h1>Olá, mundo!</h1>;
+import styles from './Heading.module.css';
+
+export function Heading(props) {
+    return <h1 className={styles.heading}>{props.children}</h1>;
 }
