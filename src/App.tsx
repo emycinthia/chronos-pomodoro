241fbfcec6921d8016a72f3087230028ce5b996f
Emily Oliveira <ewileet@gmail.com>
2025-04-30 18:33:02 -0300

    Estudo rápido hoje, a respeito de tipagem e manutenção de componentes, com algumas dicas de CSS...

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { Heading } from './components/Heading'
 import './styles/theme.css'
 import './styles/global.css'
 
 export function App() {
-    return (
+    return  (
         <> { /* --> react fragment */ }
-            <Heading attr2={123} attr='String'>
-                Olá, mundo!
-            </Heading>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum nobis qui, voluptates doloribus nam dolores dicta nisi laborum pariatur blanditiis doloremque fuga. Dolorum beatae, inventore cum tenetur quaerat cupiditate eligendi?
-            </p>
+            <div className='container'>
+                <div className='content'>
+                    <section>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, numquam expedita aspernatur amet incidunt consectetur velit illum perferendis omnis perspiciatis repudiandae minima harum, facere earum nobis deserunt, consequuntur laborum quo.
+                    </section>
+                </div>
+            </div>
         </>
-    )
+    );
 }
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,11 @@
 import styles from './Heading.module.css';
 
-export function Heading(props) {
-    return <h1 className={styles.heading}>{props.children}</h1>;
+// tipagem
+type HeadingProps = {
+    children: React.ReactNode;
+}
+
+// destructuring props > children...
+export function Heading({ children }: HeadingProps) {
+    return <h1 className={styles.heading}>{children}</h1>;
 }
